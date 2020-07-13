9d638aec7a4e2400d9a80ce3b970741b72faa824
shawn220a <shawn.42496@gmail.com>
2020-07-13 01:35:26 -0500

    Create first class, Start refactor of game

--- pythonIdleTycoon.py
+++ pythonIdleTycoon.py
@@ -1,50 +1,54 @@
 print('Welcome to Python Idle Tycoon!!!!')
 
-Money = 25.00
-Day = 1
-StoreName = 'Lemonade Stand'
-StoreCount = 0
-StoreProfit = 1.50
-StoreCost = 3
+
+class Store():
+    Money = 25.00
+    Day = 1
+
+    def __init__(self):
+        self.StoreName = 'Lemonade Stand'
+        self.StoreCount = 0
+        self.StoreProfit = 1.50
+        self.StoreCost = 3
+
+    def DisplayStoreInfo(self):
+        print('------------------------------------')
+        print('Day #' + str(Store.Day))
+        print('Money = $' + str(Store.Money))
+        print('Store Count = ' + str(self.StoreCount))
+        print('------------------------------------')
+
+    def BuyStore(self):
+        if self.StoreCost <= Store.Money:
+            self.StoreCount += 1
+            Store.Money -= self.StoreCost
+        else:
+            print('You don\'t have enough money.')
+
+    # def NextDay(self):
+    #     Store.Day += 1
+    #     DailyProfit = self.StoreProfit * self.StoreCount
+    #     Store.Money += DailyProfit
 
 
-def DisplayStoreInfo():
-    print('------------------------------------')
-    print('Day #' + str(Day))
-    print('Money = $' + str(Money))
-    print('Store Count = ' + str(StoreCount))
-    print('------------------------------------')
+newstore = Store()
+print(newstore.StoreName)
+newstore.BuyStore()
+print(newstore.StoreCount)
 
+# while True:
+#     DisplayStoreInfo()
 
-def BuyStore(StoreCount, Money):
-    if StoreCost <= Money:
-        StoreCount += 1
-        Money -= StoreCost
-    else:
-        print('You don\'t have enough money.')
-    return (StoreCount, Money)
+#     print('Available option (N)ext Day, (B)uy Store, (Q)uit')
+#     result = input('Please Enter Your Selection:')
 
-
-def NextDay(Day, Money):
-    Day += 1
-    DailyProfit = StoreProfit * StoreCount
-    Money += DailyProfit
-    return (Day, Money)
-
-
-while True:
-    DisplayStoreInfo()
-
-    print('Available option (N)ext Day, (B)uy Store, (Q)uit')
-    result = input('Please Enter Your Selection:')
-
-    if result == 'B' or result == 'b':
-        StoreCount, Money = BuyStore(StoreCount, Money)
-    elif result == 'N' or result == 'n':
-        Day, Money = NextDay(Day, Money)
-    elif result == 'Q' or result == 'q':
-        break
-    else:
-        print('Invalid Input. Try Again.')
+#     if result == 'B' or result == 'b':
+#         StoreCount, Money = BuyStore(StoreCount, Money)
+#     elif result == 'N' or result == 'n':
+#         Day, Money = NextDay(Day, Money)
+#     elif result == 'Q' or result == 'q':
+#         break
+#     else:
+#         print('Invalid Input. Try Again.')
 
 print('Thank you for playing Python Idle Tycoon')
