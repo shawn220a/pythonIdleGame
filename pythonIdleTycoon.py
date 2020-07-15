a5c3dff9953083f5f3c0d56cd24d7652f7035cbe
shawn220a <shawn.42496@gmail.com>
2020-07-14 22:04:04 -0500

    Create Multiple Stores and Format in a Table

--- pythonIdleTycoon.py
+++ pythonIdleTycoon.py
@@ -4,24 +4,32 @@ print('Welcome to Python Idle Tycoon!!!!')
 class Store():
     Money = 25.00
     Day = 1
+    StoreList = []
 
-    def __init__(self):
-        self.StoreName = 'Lemonade Stand'
+    def __init__(self, storeName, storeProfit, storeCost):
+        self.StoreName = storeName
         self.StoreCount = 0
-        self.StoreProfit = 1.50
-        self.StoreCost = 3
+        self.StoreProfit = storeProfit
+        self.StoreCost = storeCost
 
     @classmethod
     def DisplayGameInfo(cls):
         print('------------------------------------')
         print('Day #' + str(cls.Day))
-        print('Money = ${:0, .2f}'.format(cls.Money))
+        print('Money = ${:0,.2f}'.format(cls.Money))
+        print('------------------------------------')
+        print('Stores'.ljust(25) + 'Store Cost'.ljust(15) + 'Store Count')
+
+        for store in cls.StoreList:
+            store.DisplayStoreInfo()
+
         print('------------------------------------')
 
     def DisplayStoreInfo(self):
-        print('------------------------------------')
-        print('Store Name %s, Store Count = %d' % (self.StoreName, self.StoreCount))
-        print('------------------------------------')
+        StoreCostStr = '${:0,.2f}'.format(self.StoreCost).rjust(12)
+
+        print(self.StoreName.ljust(20) +
+              StoreCostStr.ljust(20) + str(self.StoreCount))
 
     def BuyStore(self):
         if self.StoreCost <= Store.Money:
@@ -36,18 +44,19 @@ class Store():
         Store.Money += DailyProfit
 
 
-CurrentStore = Store()
+Store.StoreList.append(Store('Lemonade Stand', 1.50, 3))
+Store.StoreList.append(Store('Record Store', 5, 15))
+Store.StoreList.append(Store('Ice Cream Store', 10, 90))
 
 while True:
     Store.DisplayGameInfo()
-    CurrentStore.DisplayStoreInfo()
     print('Available option (N)ext Day, (B)uy Store, (Q)uit')
     result = input('Please Enter Your Selection:')
 
     if result == 'B' or result == 'b':
-        CurrentStore.BuyStore()
+        Store.StoreList[0].BuyStore()
     elif result == 'N' or result == 'n':
-        CurrentStore.NextDay()
+        Store.StoreList[0].NextDay()
     elif result == 'Q' or result == 'q':
         break
     else:
