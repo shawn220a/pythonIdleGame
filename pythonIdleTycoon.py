382c09e0e36b71c706529e48733efaccf0670f7a
shawn220a <shawn.42496@gmail.com>
2020-07-16 00:39:37 -0500

    Working timer bar

--- pythonIdleTycoon.py
+++ pythonIdleTycoon.py
@@ -1,8 +1,33 @@
 import tkinter as tk
+from tkinter import ttk
+import time
 
 FormatMoney = '${:0,.2f}'
 
 print('Welcome to Python Idle Tycoon!!!!')
+
+
+class StoreTimer():
+    UpdateFreq = 100
+
+    def __init__(self, store):
+        self.Timer = store.Timer
+        self.store = store
+        self.StartTimer()
+
+    def StartTimer(self):
+        self.startTime = time.time()
+        root.after(StoreTimer.UpdateFreq, self.UpdateTimer)
+
+    def UpdateTimer(self):
+        elapsed = time.time() - self.startTime
+
+        if elapsed < self.Timer:
+            self.store.progressBar['value'] = elapsed / self.Timer * 100
+            root.after(StoreTimer.UpdateFreq, self.UpdateTimer)
+        else:
+            self.store.progressBar['value'] = 0
+            self.store.MakeMoney()
 
 
 class Store():
@@ -10,28 +35,35 @@ class Store():
     Day = 1
     StoreList = []
 
-    def __init__(self, storeName, storeProfit, storeCost):
+    def __init__(self, storeName, storeProfit, storeCost, storeTimer):
         self.StoreName = storeName
         self.StoreCount = 0
         self.StoreProfit = storeProfit
         self.StoreCost = storeCost
+        self.Timer = storeTimer
 
     @classmethod
     def DisplayStores(cls):
         store_label_col1 = tk.Label(root, text='Store Name')
         store_label_col1.grid(row=4, column=0)
 
-        store_label_col2 = tk.Label(root, text='Store Cost')
+        store_label_col2 = tk.Label(root, text='Progress')
         store_label_col2.grid(row=4, column=1)
 
-        store_label_col3 = tk.Label(root, text='Store Count')
+        store_label_col3 = tk.Label(root, text='Store Cost')
         store_label_col3.grid(row=4, column=2)
+
+        store_label_col4 = tk.Label(root, text='Store Count')
+        store_label_col4.grid(row=4, column=3)
 
         storeIndex = 1
 
         for store in cls.StoreList:
             store.DisplayStoreInfo(storeIndex)
             storeIndex += 1
+
+    def ClickStore(self):
+        StoreTimer(self)
 
     def MakeMoney(self):
         DailyProfit = self.StoreProfit * self.StoreCount
@@ -40,19 +72,22 @@ class Store():
 
     def DisplayStoreInfo(self, storeIndex):
         self.clickStoreButton = tk.Button(
-            root, text=self.StoreName, command=lambda: self.MakeMoney())
+            root, text=self.StoreName, command=lambda: self.ClickStore())
         self.clickStoreButton.grid(row=4 + storeIndex, column=0)
+
+        self.progressBar = ttk.Progressbar(root, value=0, maximum=100, orient=tk.HORIZONTAL, length=100, mode='indeterminate')
+        self.progressBar.grid(row=4 + storeIndex, column=1)
 
         self.storeCostLabel = tk.Label(
             root, text=FormatMoney.format(self.StoreCost))
-        self.storeCostLabel.grid(row=4 + storeIndex, column=1)
+        self.storeCostLabel.grid(row=4 + storeIndex, column=2)
 
         self.storeCountLabel = tk.Label(root, text=self.StoreCount)
-        self.storeCountLabel.grid(row=4 + storeIndex, column=2)
+        self.storeCountLabel.grid(row=4 + storeIndex, column=3)
 
         self.clickBuyButton = tk.Button(
             root, text='Buy', command=lambda: self.BuyStore())
-        self.clickBuyButton.grid(row=4 + storeIndex, column=3)
+        self.clickBuyButton.grid(row=4 + storeIndex, column=4)
 
     def BuyStore(self):
         if self.StoreCost <= Store.Money:
@@ -71,9 +106,9 @@ class GameManager():
         self.DisplayStoreList()
 
     def CreateStore(self):
-        Store.StoreList.append(Store('Lemonade Stand', 1.50, 3))
-        Store.StoreList.append(Store('Record Store', 5, 15))
-        Store.StoreList.append(Store('Ice Cream Store', 10, 90))
+        Store.StoreList.append(Store('Lemonade Stand', 1.50, 3, 3))
+        Store.StoreList.append(Store('Record Store', 5, 15, 10))
+        Store.StoreList.append(Store('Ice Cream Store', 10, 90, 30))
 
     def DisplayGameHeader(self):
         root.title('Python Idle Tycoon Business Game')
